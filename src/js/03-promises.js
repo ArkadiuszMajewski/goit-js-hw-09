b7ad451be04709aca11e811ee9db936119f0108c
ArkadiuszMajewski <119493816+ArkadiuszMajewski@users.noreply.github.com>
2023-05-13 14:31:18 +0200

    1

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+const delay = document.querySelector(`[name="delay"]`);
+const step = document.querySelector(`[name="step"]`);
+const amount = document.querySelector(`[name="amount"]`);
+const form = document.querySelector('.form');
+
+let position = 0;
+const createPromise = (position, delay) =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve('');
+      } else {
+        reject('');
+      }
+    }, delay);
+  });
+
+const resolveHandler = result => (
+  (position += 1),
+  Notiflix.Notify.success(
+    `✅ Fulfilled promise ${position} in ${new Date().getTime() - clickTime}ms`
+  )
+);
+const rejectHandler = result => (
+  (position += 1),
+  Notiflix.Notify.failure(
+    `❌ Rejected promise ${position} in ${new Date().getTime() - clickTime}ms`
+  )
+);
+
+let clickTime = 0;
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  clickTime = new Date().getTime();
+
+  for (let i = 0; i < [amount.value]; i++) {
+    createPromise(2, delay.value).then(resolveHandler).catch(rejectHandler);
   }
-}
+  position = 0;
+});
